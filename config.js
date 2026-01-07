9d2f9cb4dda05f6d805c78f97bd480284f4e3f1d
Barrett Anderson <barrett1614@gmail.com>
2026-01-07 13:01:36 -0600

    Config.js possibility

---
+++ config.js
@@ -0,0 +1,13 @@
+export const config = {
+  pairs: ["BTC/USDT", "ETH/USDT"],
+  exchanges: {
+    binance: {
+      fee: 0.001, // 0.1% trading fee
+      withdrawal: { BTC: 0.0005, ETH: 0.005 }, // ESTIMATED withdrawal fees
+    },
+    kraken: {
+      fee: 0.0026, // 0.26% trading fee
+      withdrawal: { BTC: 0.0005, ETH: 0.005 },
+    },
+  },
+};
